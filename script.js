a338f0cdff7a9493ce8963108d48605ee6382fc2
Fatima Cabrera <160612045+facarce7@users.noreply.github.com>
2024-02-29 15:52:49 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,34 +1,14 @@
-let palabra = "APPLE"; 
+ 
 let intentos = 6;
-let verde = "#79b851"
-let amarillo= "#f3c237"
-let gris = "#a4aec4"
+let diccionario = ['APPLE', 'HURLS', 'WINGS', 'YOUTH']
+
+const palabra = diccionario[Math.floor(Math.random() * diccionario.length)];
 
 const BUTTON = document.getElementById("guess-button");
 
 BUTTON.addEventListener("click", intentar);
 console.log(palabra);
 
-const GRID = document.getElementById("grid");
-    const ROW = document.createElement('div');
-    ROW.className = 'row';
-    const INTENTO = "[i]"
-    for (let i in palabra){
-        const SPAN = document.createElement('span');
-        SPAN.className = 'letter';
-        if (INTENTO[i]===palabra[i]){ //VERDE
-            SPAN.innerHTML = INTENTO[i];
-            SPAN.style.backgroundColor = 'green';
-        } else if( palabra.includes(INTENTO[i]) ) { //AMARILLO
-            SPAN.innerHTML = INTENTO[i];
-            SPAN.style.backgroundColor = 'yellow';
-        } else {      //GRIS
-            SPAN.innerHTML = INTENTO[i];
-            SPAN.style.backgroundColor = 'grey';
-        }
-        ROW.appendChild(SPAN)
-    }
-    GRID.appendChild(ROW)
 
 function leerIntento(){
     let intento = document.getElementById("guess-input");
@@ -37,13 +17,34 @@ function leerIntento(){
     if(intento.length !=5){
         alert("Debe ingresar una palabra de 5 letras");
     } else{
-        return intento;
+            return intento
+        }
     }
-}
 
-for (let i in palabra){
-	console.log(palabra[i]);
-}
+    for (let i in palabra){
+        console.log(palabra[i]);
+    }
+
+    function intentar(){
+        const INTENTO = leerIntento();
+        if (INTENTO === palabra ) {
+            terminar ("<h1>GANASTE</h1>")
+            return
+        }
+        for (let i in palabra){
+            if (INTENTO[i]===palabra[i]){
+                console.log(INTENTO[i], "VERDE")
+            } else if( palabra.includes(INTENTO[i]) ) {
+                console.log(INTENTO[i], "AMARILLO")
+            } else {
+                console.log(INTENTO[i], "GRIS")
+            }
+        }
+            intentos--
+        if (intentos==0){
+            terminar("<h1>PERDISTE!</h1>")
+        }
+    }
 
 function terminar (mensaje){
     const INPUT = document.getElementById ("guess-input");
@@ -53,25 +54,34 @@ function terminar (mensaje){
     contenedor.innerHTML = mensaje;
 }
 
-function intentar(){
+const GRID = document.getElementById("grid"); 
+
+function intentar() {
     const INTENTO = leerIntento();
-    if (INTENTO === palabra ) {
-        terminar ("<h1>GANASTE!</h1>")
-        return
+    if (INTENTO === palabra) {
+        terminar("<h1>GANASTE</h1>");
+        return;
     }
-    for (let i in palabra){
-        if (INTENTO[i]===palabra[i]){
-            console.log(INTENTO[i], "VERDE")
-        } else if( palabra.includes(INTENTO[i]) ) {
-            console.log(INTENTO[i], "AMARILLO")
+    const ROW = document.createElement("div");
+    ROW.className = "row";
+    for (let i in palabra) {
+        const SPAN = document.createElement("span");
+        SPAN.className = "letter";
+        if (INTENTO[i] === palabra[i]) {
+            SPAN.innerHTML = INTENTO[i];
+            SPAN.style.backgroundColor = "#79b851";
+        } else if (palabra.includes(INTENTO[i])) {
+            SPAN.innerHTML = INTENTO[i];
+            SPAN.style.backgroundColor = "#f3c237";
         } else {
-            console.log(INTENTO[i], "GRIS")
+            SPAN.innerHTML = INTENTO[i];
+            SPAN.style.backgroundColor = "#a4aec4";
         }
+        ROW.appendChild(SPAN);
     }
-		intentos--
+    GRID.appendChild(ROW);
+    intentos--
     if (intentos==0){
         terminar("<h1>PERDISTE!</h1>")
     }
 }
-
-
